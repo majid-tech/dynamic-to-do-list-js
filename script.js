415aa67956040e782823b0b2107f20385f4f1b09
majid-tech <majidmohammed044@gmail.com>
2025-12-03 23:18:16 +0000

    todo-lizt app

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const addButton = document.getElementById('add-task-btn')
+    const taskInput = document.getElementById('task-input')
+    const taskList = document.getElementById('task-list')
+
+    function addTask(){
+        const taskText = taskInput.value.trim()
+
+        if(taskText === ''){
+            alert("Enter a task")
+        } else {
+            const li = document.createElement('li')
+            li.textContent = taskText
+
+            const removeBtn = document.createElement('button')
+            removeBtn.textContent = 'Remove'
+            removeBtn.className = 'remove-btn'
+
+            removeBtn.addEventListener('click', () => {
+                li.remove()
+            })
+
+            li.appendChild(removeBtn)
+            taskList.appendChild(li)
+
+            taskInput.value = ''
+        }
+    }
+
+    addButton.addEventListener('click', addTask)
+    taskInput.addEventListener('keypress', (event) => {
+        if(event.key === "Enter"){
+            addTask()
+        }
+    }
+    )
+
+})
